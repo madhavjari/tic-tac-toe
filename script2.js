b19293634459115688495232f52889ddbac51bed
madhavjari <213913746+madhavjari@users.noreply.github.com>
2025-09-04 19:27:57 +0530

    all logical part done

--- script2.js
+++ script2.js
@@ -1,29 +1,71 @@
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
+
 function createPlayer(name,marker){
-    return{name,marker};
+    array = [];
+    return{name,marker,array};
+}
+
+function gameBoard(){
+    const startGame = () =>{
+        let board = [];
+        for(let i = 0; i < 9; i++){
+            board[i] = document.createElement('button');
+            board[i].textContent = '';
+            board[i].setAttribute('data-column',i+1);
+            container.appendChild(board[i]);
+        }
+    }
+    const winningCombination = [[1,4,7],[1,5,9],[1,2,3],[2,5,8],[3,6,9],[4,5,6],[7,8,9],[3,5,7]]
+    const getBoard = () => board;
+    const resetBoard = () => {
+        container.innerHTML = '';
+        startGame();
+    }
+    return{getBoard, winningCombination,resetBoard, startGame};
+}
+
+function resultCheck(winningCombination,array){
+    return winningCombination.some(combination => {
+        return combination.every(element => array.includes(element));
+  });
 }
 
 function gameController(){
-    player1 = 'Madhav';
-    player2 = 'Shreya';
+    const player1 = 'Madhav';
+    const player2 = 'Shreya';
     const players = [createPlayer(player1,'X'),createPlayer(player2,'O')];
     let activePlayer = players[0];
     const switchPlayerTurn = () => activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    let array = ['1','2','3','4','5','6','7','8','9'];
-    const clickedButton = document.querySelector('.container');
-    let position;
-    clickedButton.addEventListener('click',(event) =>{ 
-        const clickedElement = event.target;
-        position = clickedElement.textContent;
-        if(array.includes(position)){
-            const index = array.indexOf(position);
-            if(index!== -1) array.splice(index,1);
-            clickedElement.textContent = activePlayer.marker;
-            switchPlayerTurn();
-        };
-    });
-    return{position,activePlayer};
+    const board = gameBoard();
+    board.startGame();
+    function attachListeners(){
+        const clickedButton = document.querySelectorAll('button');
+        clickedButton.forEach(button => {
+            button.addEventListener('click',(event) =>{ 
+                console.log(activePlayer);
+                const clickedElement = event.target;
+                clickedElement.textContent = activePlayer.marker;
+                clickedElement.disabled = true;
+                activePlayer.array.push(parseInt(clickedElement.dataset.column));  
+                const result = resultCheck(board.winningCombination,activePlayer.array);
+                if(result){
+                    alert(`${activePlayer.name} has won`);
+                    resetGame();
+                    return;
+                }
+                switchPlayerTurn();
+            });
+        });
+    }
+    function resetGame(){
+        players.forEach(player => player.array=[]);
+        board.resetBoard();
+        attachListeners();
+    }
+    attachListeners();
 }
-
-const clickedButton = document.querySelector('.container');
 
 const game = gameController();
